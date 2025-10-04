ff97a7462bf69b34c7288dc4d51b741215099485
Vestamart <117672151+Vestamart@users.noreply.github.com>
2025-10-04 14:47:46 +0300

    load l1_2

---
+++ l1_2/main.go
@@ -0,0 +1,24 @@
+package l1_2
+
+import (
+	"fmt"
+	"sync"
+)
+
+func Start() {
+	numbers := []int{2, 4, 6, 8, 10}
+
+	var wg sync.WaitGroup
+
+	for _, number := range numbers {
+		wg.Add(1)
+		go func(n int) {
+			defer wg.Done()
+			square := n * n
+			fmt.Printf("%d в квадрате = %d\n", n, square)
+		}(number)
+	}
+
+	wg.Wait()
+
+}
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/Vestamart/wb-l1/l1_1"
+import (
+	"github.com/Vestamart/wb-l1/l1_1"
+	"github.com/Vestamart/wb-l1/l1_2"
+)
 
 func main() {
 	l1_1.Start()
+	l1_2.Start()
 }
